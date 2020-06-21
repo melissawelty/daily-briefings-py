88a9347c1c2ce060c40907da53ea5bbf384a44ba
melissawelty <65548626+melissawelty@users.noreply.github.com>
2020-06-21 18:56:59 -0500

    rendering HTML pages

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/home_routes.py
 
-from flask import Blueprint #, render_template
+from flask import Blueprint, render_template
 
 home_routes = Blueprint("home_routes", __name__)
 
@@ -8,12 +8,15 @@ home_routes = Blueprint("home_routes", __name__)
 def index():
     print("VISITED THE HOME PAGE")
     #return render_template("dashboard.html")
-    return "Welcome Home (TODO)"
+    #return "Welcome Home (TODO)"
+    return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("VISITED THE ABOUT PAGE")
-    return "About Me (TODO)"
+    # return "About Me (TODO)"
+    return render_template("about.html")
+
 
 @home_routes.route("/register")
 def register():
